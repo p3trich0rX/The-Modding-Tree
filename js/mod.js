7f1e7e28f774d5358b193c7a086aa460393e6257
p3trich0rX <barancanbilirege@gmail.com>
2024-03-23 15:51:17 +0300

    0.alpha
    learning shit

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "Fikibok Tree",
+	id: "fikimod",
+	author: "Petrichor",
+	pointsName: "haga points",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
